91145d01705d01d3536a445c83b7e7fe6a3711e4
Lingerew Getie <57194552+Lingerew711@users.noreply.github.com>
2021-09-10 11:34:19 -0900

    changed to run with environment port number

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use("/api/auth", Routes.authRoute);
 app.use("/api/property/",Routes.propertyRoute);
 
 
-//lines below serve files inside uploads directory and make them accessible through http://localhost:3000/filename
+//lines below serve files inside uploads directory and make them accessible through http://localhost:port/filename
 app.use(express.static(__dirname + '/public'));
 app.use(express.static('uploads'));
 app.use(errorMiddleware);
